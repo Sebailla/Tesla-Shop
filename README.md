55546fdfd4781ec99d5cb77cb8dcf17cd4982883
Sebailla <seba.illa.1977@gmail.com>
2025-03-27 15:59:05 -0300

    Add new files and update README and package configurations

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
   <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="120" alt="Nest Logo" /></a>
 </p>
 
-#Tesla Shop API
+# Tesla Shop API
+
 
 ## Ejecutar en desarrollo:
 
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+
+    ConfigModule.forRoot(),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : (() => {
+        throw new Error("DB_PORT is not defined in environment variables.");
+      })(),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
